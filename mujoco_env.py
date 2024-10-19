25654fd7eeb83e9e7c156caefabedc8bd42d61f6
Abhilash Joshi <107572032+AbhilashJoshi25@users.noreply.github.com>
2024-10-18 21:08:19 -0700

    Create mujoco_env.py

---
+++ mujoco_env.py
@@ -0,0 +1,24 @@
+import gymnasium as gym
+
+# WRITE YOUR CODE HERE
+# Create the InvertedPendulum-v4 environment with rendering
+env = gym.make('InvertedPendulum-v4', render_mode='human')
+print(env.action_space)
+print(env.observation_space)
+
+obs, _ = env.reset()
+env.render()
+
+done = False
+episode_length = 0
+total_reward = 0
+
+while not done:       
+    action = env.action_space.sample()   
+    obs, reward, terminated, truncated, _ = env.step(action)
+    episode_length += 1
+    total_reward += reward   
+    done = terminated or truncated         
+
+print(f'Episode length: {episode_length}, total reward: {total_reward:.2f}')
+env.close()
